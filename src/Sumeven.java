14b8b59f654140b0a0aab9006dcd2e19eba0e56a
monikasri <monikasrimonika25@gmail.com>
2026-06-24 23:43:04 +0530

    First Commit

---
+++ src/Sumeven.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Sumeven {
+    public static void main(String[] args) {
+        int sum = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your number:");
+        int n = sc.nextInt();
+        for (int i = 1; i <= n-1; i++) {
+            if (i % 2 == 0) {
+                sum = sum + i;
+            }
+        }
+        System.out.println(sum);
+    }
+}
+
