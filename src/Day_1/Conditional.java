d1309dd807aebb7a92fd1006c3478b94818a0bdf
monikasri <monikasrimonika25@gmail.com>
2026-06-25 10:30:55 +0530

    check a number is greaterthan 10 or 50 or 90

---
+++ src/Day_1/Conditional.java
@@ -0,0 +1,23 @@
+package Day_1;
+
+import java.util.Scanner;
+
+public class Conditional {
+        public static void main(String[] args){
+            Scanner sc=new Scanner(System.in);
+            System.out.println("Enter the number:");
+            int n=sc.nextInt();
+
+
+            if(n>90){
+                System.out.println("Greaterthan 90");
+            }
+            else if(n>50){
+                System.out.println("Greaterthan 50");
+            }
+            else{
+                System.out.println("Greaterthan 10");
+            }
+        }
+    }
+
